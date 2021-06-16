1f838aff8316a1020616bb4c19ec7a73ea7148f7
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-16 18:27:53 +0200

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,5 +1,5 @@
 test('sanity', () => {         // test
   expect(true).not.toBe(false) // assertion
   expect(1 + 2).toBe(3)        // another assertion
-  // expect('foo').               // toBe is a "matcher"
+  expect('foo').toHaveLength(3)               // toBe is a "matcher"
 })
